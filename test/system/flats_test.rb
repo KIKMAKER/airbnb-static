9062e3ecb0abfe01e34788b2d19a03e2b0a2d2f9
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-11-13 18:34:28 +0200

    with pictures

---
+++ test/system/flats_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class FlatsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit "/"
+
+    assert_selector "h1", text: "Flat"
+    assert_selector "h2", text: "Beautiful"
+
+  end
+end
